48087a86059eca83927c28e39a22be38753911f3
Matheus F <13111055+SeikiMatt@users.noreply.github.com>
2023-09-01 22:37:38 -0300

    make sure canvas renders in browser

--- src/lib/components/Canvas.svelte
+++ src/lib/components/Canvas.svelte
@@ -1,5 +1,8 @@
 <script lang="ts">
+	import { browser } from '$app/environment';
 	export let canvas: HTMLCanvasElement
 </script>
 
-<canvas bind:this={canvas} width={screen.availWidth} height={screen.availHeight}/>
+{#if browser}
+	<canvas bind:this={canvas} width={screen.availWidth} height={screen.availHeight}/>
+{/if}
