d892f08731e1d76c644681d584e79a4342a34cbe
bermudalocket <github@eigenstuff.net>
2023-09-03 22:36:33 -0400

    trigger hmr when glsl files are updated

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,21 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
-	plugins: [sveltekit()],
+	plugins: [
+		sveltekit(),
+		{
+			name: 'glsl-hmr',
+			enforce: 'post',
+			handleHotUpdate({ file, server }) {
+				if (file.endsWith('.glsl')) {
+					server.ws.send({
+						type: 'full-reload',
+						path: '*'
+					});
+				}
+			}
+		},
+	],
 	test: {
 		include: ['test/**/*.{test,spec}.{js,ts}']
 	}
